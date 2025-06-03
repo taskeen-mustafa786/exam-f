dee6a751ced221d982607f98a59beb7ca5191fa6
taskeen-mustafa <taskeenmustafanizamani.bscsf22@iba-suk.edu.pk>
2025-06-03 11:51:56 +0500

    finalized task done as much as i can

--- final/src/App.jsx
+++ final/src/App.jsx
@@ -1,17 +1,4 @@
 import { useEffect, useState } from 'react'
-import './App.css'
-
-// Use the provided GIF to observe:
-// The sequence of light changes (Red → Yellow → Green → Red...)
-// Each light stays active for 3 seconds
-// Only one light is active at a time
-// Smooth transitions and style
-
-//  Light Logic
-// Use useState to track the current light (red/yellow/green)
-// Use useEffect with setInterval to change light every 3 seconds
-// Apply active CSS class to the current light
-
 
 function App() {
   const [count, setCount] = useState(0)
@@ -22,7 +9,7 @@ function App() {
       setCount(prevCount => (prevCount + 1) % 3)
     }, 3000)
 
-    // Clear the interval on component unmount
+  
     return () => clearInterval(interval)
 
 
@@ -30,10 +17,10 @@ function App() {
 
   return (
     <>
-      <div className='bg-gray-800 p-4 rounded-2xl'>
-        <div className={` rounded-full h-12 w-12 p-2 m-2 ${count==0?'bg-red-600 active':'bg-gray-600'}`}></div>
-        <div className={` rounded-full h-12 w-12 p-2 m-2 ${count==1?'bg-yellow-600 active':'bg-gray-600'}`}></div>
-        <div className={` rounded-full h-12 w-12 p-2 m-2 ${count==2?'bg-green-600 active':'bg-gray-600'} `}></div>
+      <div className='bg-gray-800 p-4 rounded-2xl transition-flat'>
+        <div className={`  rounded-full h-12 w-12 p-2 m-3 bg-repeat-round  ${count==0?'bg-red-600 shadow-red-500 shadow-lg  active':'bg-gray-600'}`}></div>
+        <div className={` rounded-full h-12 w-12 p-2 m-3 ${count==1?'bg-yellow-600 shadow-yellow-500 shadow-lg active':'bg-gray-600'}`}></div>
+        <div className={` rounded-full h-12 w-12 p-2 m-3 ${count==2?'bg-green-600 shadow-green-500 shadow-lg active':'bg-gray-600'} `}></div>
       </div>
     </>
   )
@@ -41,19 +28,4 @@ function App() {
 
 export default App
 
-// Commit # Commit Message
-// Initial commit: add static HTML and CSS
-// for traffic light
-// Refactor: convert to functional React
-// component
-// Feature: implement light cycle logic 3
-// with useEffect
-// Style: enhance UI with transitions and 4
-// active styles
-// Task
-// Setup project and layout (3
-// lights, no logic)
-// JSX structure for lights, apply
-// classes
-// Cycle lights every 3s using state
-// + effect
+
